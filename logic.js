8b2bf4b8a2cc99400625c139dc9f132cbd54d158
keshavmehra79 <mehrakeshav332@gmail.com>
2025-07-19 11:43:23 +0530

    19july

--- logic.js
+++ logic.js
@@ -1,54 +1,54 @@
+
+        //LOGIN-SINGUP LOCALUSER
+
 function fordata(){
-    let name=document.querySelector('#name').value
-    if(name==""){
+    let LOCALUSER={
+        name:document.querySelector('#name').value,
+        email:document.querySelector('#email').value,
+        contact:document.querySelector('#contact').value,
+        city:document.querySelector('#city').value,
+        password:document.querySelector('#password').value
+    }
+    if(LOCALUSER.name==""){
         alert("Please Enter Your name")
         document.querySelector('#name').focus()
         return false
     }
-    localStorage.setItem('Username',name)
 
-    let email=document.querySelector('#email').value;
-        if(email==""){
+        if(LOCALUSER.email==""){
         alert("Please Enter your email")
         document.querySelector('#email').focus()
         return false
     }
-    if(!(email.includes('@gmail.com') || email.includes('@yahoo.com'))){
+    if(!(LOCALUSER.email.includes('@gmail.com') || LOCALUSER.email.includes('@yahoo.com'))){
         alert("Please Enter your valid email")
         document.querySelector('#email').focus()
         return false
     }
-    localStorage.setItem('Email',email)
 
-    let contact=document.querySelector('#contact').value
-    if(contact==""){
+    if(LOCALUSER.contact==""){
         alert("please Enter your contact number")
         document.querySelector('#contact').focus()
         return false
     }
-    localStorage.setItem('Contact',contact)
 
-    let password=document.querySelector('#password').value
-    if(password==""){
+    if(LOCALUSER.password==""){
         alert("Please Make your password")
         document.querySelector('#password').focus()
         return false 
     }
 
-    else if(!(password.match(/[!@#$%^&*]/))){
+     if(!(LOCALUSER.password.match(/[!@#$%^&*]/))){
         alert('Please Make Strong Password (e.g-@abcd#')
         document.querySelector('#password').focus()
         return false
     }
-    localStorage.setItem('password',password)
-    let city=document.querySelector('#city').value
-    if(city==""){
+    if(LOCALUSER.city==""){
         alert("Please Enter your city")
         document.querySelector('#city').focus()
         return false
     }
-    localStorage.setItem('City',city)
-   
+   localStorage.setItem('LOCALUSER',JSON.stringify(LOCALUSER))
 
 }
 
@@ -63,7 +63,7 @@ function login(){
         return false
     }
 }
-
+        //LOGIN-SIGNUP ADMIN
 function admin(){
     let admindata={
         name:document.querySelector('#adminname').value,
@@ -72,14 +72,66 @@ function admin(){
         contact:document.querySelector('#admincontact').value,
         password:document.querySelector('#adminpassword').value
     }
+    if(admindata.name==""){
+        alert("Please Your Name")
+        document.querySelector('#adminname').focus()
+        return false
+    }
+    else if(admindata.email==""){
+        alert("Please enter your Email")
+        document.querySelector('#adminemail').focus()
+        return false
+    }
+    else if(!(admindata.email.includes('@gmail.com') || (admindata.email.includes('@yahoo.com')))){
+        alert('Please enter your valid email')
+        document.querySelector('#adminemail').focus()
+        return false
+    }
+    else if(admindata.city==""){
+        alert('Please Enter Your city')
+        document.querySelector('#admincity').focus()
+        return false
+    }
+    else if(admindata.contact==""){
+        alert('Enter your contact number')
+        document.querySelector('#admincontact').focus()
+        return false
+    }
+    else if(admindata.contact.length!=10){
+        alert('Please enter 10 digit contact')
+        document.querySelector('#admincontact').focus()
+        return false
+    }
+    else if(admindata.password==""){
+        alert('Please make password')
+        document.querySelector('#adminpassword').focus()
+        return false
+    }
+    else if(!(admindata.password.match(/[!@#$%^&*]/))){
+        alert('Plaese Make strong password')
+        document.querySelector('#adminpassword').focus()
+        return false
+    }
     localStorage.setItem('Admindata',JSON.stringify(admindata))
-   
-    
-    
-    
+      
+      
 }
- let adminstorage=JSON.parse(localStorage.getItem('Admindata'))
-    console.log(adminstorage);
-    let adminname=document.querySelector('#admin')
-    adminname.textContent=adminstorage.name
+   let data=localStorage.getItem('Admindata')
+   console.log(data);
+   
+
+
+function loginadmin(){
+    let data=JSON.parse(localStorage.getItem('Admindata'))
+    let logemail=document.querySelector('#logadmin').value
+    let logpass=document.querySelector('#passadmin').value
+    if(logemail!=data.email || logpass!=data.password){
+        alert('User not found!please try again')
+        document.querySelector('#logadmin').focus()
+        return false
+    }
+
+
+} 
+
 
